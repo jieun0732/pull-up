d548103c57f489c93c4ab2b97faa10bbbfffcd2d
장재성 <lancer98@naver.com>
2024-08-02 19:52:47 +0900

    [BE] feat: 카카오 로그인 구현

--- backend/pull-up/src/main/java/pull_up/api/member/repository/MemberRepository.java
+++ backend/pull-up/src/main/java/pull_up/api/member/repository/MemberRepository.java
@@ -1,5 +1,6 @@
 package pull_up.api.member.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pull_up.api.member.entity.Member;
 
@@ -8,4 +9,5 @@ import pull_up.api.member.entity.Member;
  */
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    Optional<Member> findByEmail(String email);
 }
