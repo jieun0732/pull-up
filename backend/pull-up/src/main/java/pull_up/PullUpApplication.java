ebec013369f91490475f03417cc782e25667c81c
장재성 <lancer98@naver.com>
2024-08-21 19:00:44 +0900

    [BE] fix: SwaggerConfig 수정3

--- backend/pull-up/src/main/java/pull_up/PullUpApplication.java
+++ backend/pull-up/src/main/java/pull_up/PullUpApplication.java
@@ -1,8 +1,14 @@
 package pull_up;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(
+	servers = {
+		@Server(url="/", description = "Default Server url")
+	}
+)
 @SpringBootApplication
 public class PullUpApplication {
 
