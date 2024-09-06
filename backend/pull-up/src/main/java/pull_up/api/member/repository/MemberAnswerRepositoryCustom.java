a0c1e760b6e643de9f34072ea1cae3843847d6a0
장재성 <lancer98@naver.com>
2024-09-06 17:55:17 +0900

    [BE] fix: 파일 경로 변경

--- backend/pull-up/src/main/java/pull_up/api/member/repository/MemberAnswerRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package pull_up.api.member.repository;
-
-import java.util.List;
-import pull_up.api.member.entity.MemberAnswer;
-
-public interface MemberAnswerRepositoryCustom {
-    List<MemberAnswer> findByMemberAndOptionalFilters(Long memberId, String entry, String category, String type);
-}
-
