8fb49d848e7074ae75aa4c8215ffcdfd465c54f0
whereami0404 <whereami2048@gmail.com>
2024-10-10 22:32:30 +0900

    KAN-18 [BE] fix: converter 수정

--- backend/src/main/java/pull_up/global/Oauth/CustomRequestEntityConverter.java
+++ backend/src/main/java/pull_up/global/Oauth/CustomRequestEntityConverter.java
@@ -1,16 +1,17 @@
 package pull_up.global.Oauth;
 
-import io.jsonwebtoken.JwsHeader;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
 import java.security.PrivateKey;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.apache.commons.io.IOUtils;
 import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
 import org.bouncycastle.openssl.PEMParser;
 import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
@@ -19,71 +20,65 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.RequestEntity;
 import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
 import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequestEntityConverter;
-import org.springframework.stereotype.Component;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.util.MultiValueMap;
 
 @Slf4j
 public class CustomRequestEntityConverter implements Converter<OAuth2AuthorizationCodeGrantRequest, RequestEntity<?>> {
-
     private final OAuth2AuthorizationCodeGrantRequestEntityConverter defaultConverter;
+    private final String path;
+    private final String keyId;
+    private final String teamId;
+    private final String clientId;
+    private final String url;
 
-    public CustomRequestEntityConverter() {
-        defaultConverter = new OAuth2AuthorizationCodeGrantRequestEntityConverter();
+    public CustomRequestEntityConverter(AppleProperties properties) {
+        this.defaultConverter = new OAuth2AuthorizationCodeGrantRequestEntityConverter();
+        this.path = properties.getPath();
+        this.keyId = properties.getKid();
+        this.teamId = properties.getTid();
+        this.clientId = properties.getCid();
+        this.url = properties.getUrl();
     }
-
     @Override
     public RequestEntity<?> convert(OAuth2AuthorizationCodeGrantRequest req) {
         RequestEntity<?> entity = defaultConverter.convert(req);
         String registrationId = req.getClientRegistration().getRegistrationId();
         MultiValueMap<String, String> params = (MultiValueMap<String, String>) entity.getBody();
 
-        // Apple일 경우 secret key를 동적으로 세팅
         if (registrationId.contains("apple")) {
-            assert params != null;
-            params.set("client_secret", createAppleClientSecret(params.get("client_id").get(0), params.get("client_secret").get(0)));
+            try {
+                params.set("client_secret", createClientSecret());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
+        return new RequestEntity<>(params, entity.getHeaders(),
+                entity.getMethod(), entity.getUrl());
+    }
+    public PrivateKey getPrivateKey() throws IOException {
 
-        return new RequestEntity<>(params, entity.getHeaders(), entity.getMethod(), entity.getUrl());
+        ClassPathResource resource = new ClassPathResource(path);
+
+        InputStream in = resource.getInputStream();
+        PEMParser pemParser = new PEMParser(new StringReader(IOUtils.toString(in, StandardCharsets.UTF_8)));
+        PrivateKeyInfo object = (PrivateKeyInfo) pemParser.readObject();
+        JcaPEMKeyConverter converter = new JcaPEMKeyConverter();
+        return converter.getPrivateKey(object);
     }
 
-    public String createAppleClientSecret(String clientId, String secretKeyResource) {
-        String clientSecret = "";
-        String[] secretKeyResourceArr = secretKeyResource.split("/");
+    public String createClientSecret() throws IOException {
+        Map<String, Object> jwtHeader = new HashMap<>();
+        jwtHeader.put("kid", keyId);
+        jwtHeader.put("alg", "ES256");
 
-        try {
-            InputStream inputStream = new ClassPathResource("static/apple/" + secretKeyResourceArr[0]).getInputStream();
-            File file = File.createTempFile("appleKeyFile", ".p8");
-            try {
-                FileCopyUtils.copy(inputStream.readAllBytes(), file);
-            } finally {
-                IOUtils.closeQuietly(inputStream);
-            }
-
-            String appleKeyId = secretKeyResourceArr[1];
-            String appleTeamId = secretKeyResourceArr[2];
-            String appleClientId = clientId;
-
-            PEMParser pemParser = new PEMParser(new FileReader(file));
-            JcaPEMKeyConverter converter = new JcaPEMKeyConverter();
-            PrivateKeyInfo privateKeyInfo = (PrivateKeyInfo) pemParser.readObject();
-
-            PrivateKey privateKey = converter.getPrivateKey(privateKeyInfo);
-
-            clientSecret = Jwts.builder()
-                .setHeaderParam(JwsHeader.KEY_ID, appleKeyId)
-                .setIssuer(appleTeamId)
-                .setAudience("https://appleid.apple.com")
-                .setSubject(appleClientId)
-                .setExpiration(new Date(System.currentTimeMillis() + (1000 * 60 * 5)))
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .signWith(privateKey, SignatureAlgorithm.ES256)
+        return Jwts.builder()
+                .setHeaderParams(jwtHeader)
+                .setIssuer(teamId)
+                .setIssuedAt(new Date(System.currentTimeMillis())) // 발행 시간 - UNIX 시간
+                .setExpiration(new Date(System.currentTimeMillis() + (1000 * 60 * 5)))// 만료 시간
+                .setAudience(url)
+                .setSubject(clientId)
+                .signWith(getPrivateKey(), SignatureAlgorithm.ES256)
                 .compact();
-
-        } catch (IOException e) {
-            log.error("Error_createAppleClientSecret : {}-{}", e.getMessage(), e.getCause());
-        }
-        log.info("createAppleClientSecret : {}", clientSecret);
-        return clientSecret;
     }
 }
