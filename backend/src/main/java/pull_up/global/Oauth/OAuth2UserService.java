0cfb981efdc523194a8b6af120f4f5a2df4a9db8
whereami2048 <whereami2048@gmail.com>
2024-10-12 01:53:22 +0900

    [BE] chore: 애플 로그인 콜백 데이터 테스트

--- backend/src/main/java/pull_up/global/Oauth/OAuth2UserService.java
+++ backend/src/main/java/pull_up/global/Oauth/OAuth2UserService.java
@@ -6,10 +6,12 @@ import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import pull_up.api.member.entity.Member;
@@ -67,7 +69,7 @@ public class OAuth2UserService extends DefaultOAuth2UserService {
             .getUserNameAttributeName();
 
         log.info("principle " + new CustomUserDetails(member, attributes, userNameAttributeName));
-        return new CustomUserDetails(member, attributes, userNameAttributeName);
+        return new CustomUserDetails(member, attributes, Objects.requireNonNullElse(userNameAttributeName, "APPLE_NULL"));
     }
 
     //JWT Payload부분 decode 메서드
