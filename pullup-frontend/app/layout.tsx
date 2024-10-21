7062e5b6a9cf3f2b616ed80406a215a94c01aab6
“seunghui_kim” <seunghui_kim@tmax.co.kr>
2024-10-21 20:51:15 +0900

    [FE] feat. google analytics 컴포넌트 추가 (KAN-21)

--- pullup-frontend/app/layout.tsx
+++ pullup-frontend/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import GoogleAnalytics from "./lib/GoogleAnalytics";
 // import { Inter } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -23,8 +24,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={pretendard.className}>{children}</body>
+    <html lang="ko">
+      <body className={pretendard.className}>
+        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
+          <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
+        ) : null}
+        {/* 상태관리 컨텍스트 등은 여기서 감싸주면 됨 */}
+        {children}
+      </body>
     </html>
   );
 }
