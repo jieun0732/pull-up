ce27e2900e44936aa84bd99a08f2cb37791d2249
SangyeopNam <spearoad15@gmail.com>
2024-11-06 15:27:18 +0900

    fix: 애플 로그인 시 이미 등록된 회원 다시 불러오기2

--- backend/src/main/java/pull_up/global/auth/v2/exception/OAuthError.java
+++ backend/src/main/java/pull_up/global/auth/v2/exception/OAuthError.java
@@ -10,7 +10,6 @@ public enum OAuthError {
     REQUEST_CONVERT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Converted Entity is Null. Might occurred convert problem with OAuth Request to Request Entity."),
     PARSE_APPLE_PUBLIC_KEY_ERROR(HttpStatus.UNAUTHORIZED, "Error has been occurred to get Apple Public Key."),
     NOT_REGISTERED_MEMBER_IN_DATABASE(HttpStatus.BAD_REQUEST, "Not Registered Member, But Apple response doesn't have User json."),
-    ALREADY_REGISTERED_MEMBER_WITH_USER_JSON(HttpStatus.BAD_REQUEST, "Already Registered Member, But Apple response has User json."),
     NOT_PROVIDED_OAUTH2_VENDOR_REQUEST(HttpStatus.BAD_REQUEST, "Not Provided Vendor Request."),
     APPLE_USER_PARSE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "Apple User parse throws exception."),
     PARSE_JWT_ERROR(HttpStatus.UNAUTHORIZED, "Invalid Login Token."),
